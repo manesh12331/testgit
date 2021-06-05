a7bc7d53a0c675e1bd85795e3571f11d67b5f1ec
manesh12331 <maneshpunde69@gmail.com>
2021-06-05 20:39:40 +0530

    first page

--- src/components/first.jsx
+++ src/components/first.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
- class first extends Component {
+ class First extends Component {
     render() {
         return (
             <div>
@@ -10,4 +10,4 @@ import React, { Component } from 'react'
     }
 }
 
-export default first;
+export default First;
