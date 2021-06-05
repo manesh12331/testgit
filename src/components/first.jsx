950cba97673a1851e092441d0fa3d8644eaaad38
manesh12331 <maneshpunde69@gmail.com>
2021-06-05 20:37:07 +0530

    some changes

---
+++ src/components/first.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class first extends Component {
+    render() {
+        return (
+            <div>
+               <h1>In first page </h1>
+            </div>
+        )
+    }
+}
+
+export default first;
