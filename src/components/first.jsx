8e5f293eb996b3e6d716d7ac3e8ee5ff8fc6abe1
manesh12331 <maneshpunde69@gmail.com>
2021-06-09 16:11:39 +0530

    change

--- src/components/first.jsx
+++ src/components/first.jsx
@@ -5,6 +5,7 @@ import React, { Component } from 'react'
         return (
             <div>
                <h1>In first page </h1>
+               <p>manesh@gmail.com</p>
             </div>
         )
     }
